79e0b27d92b90d702ed3f47fa6b9e558226766b9
Manish <manishkumargupta97099@gmail.com>
2026-08-24 18:48:16 +0530

    added new code

---
+++ pipeline_code/provider.tf
@@ -0,0 +1,19 @@
+terraform{
+    required_providers{
+        azurerm={
+            source="hashicorp/azurerm"
+            version="5.0.0"
+        }
+    }
+
+    backend "azurerm"{
+        storage_account_name = "stgchor"
+        container_name = "containerchor"
+        resource_group_name = "rg_chor"
+        key = ""
+    }
+}
+
+provider "azurerm" {
+  features{}
+}
---
+++ pipeline_code/terraform.tfvars
@@ -0,0 +1,17 @@
+rgs={
+    "rg1" = {
+        name = "cheetah_rg"
+        location = "Japan East"
+    }
+}
+
+stgs={
+    "stg1" = {
+        name= "storagelocal"
+        location= "Japan East"
+        resource_group_name= "cheetah_rg"
+        account_replication_type = "LRS"
+        account_tier = "Standard"
+        
+    }
+}
